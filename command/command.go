65de7ec6c2d30891f058c497da809c25d1aadc02
Mourey jerome <jerome@everycheck.fr>
2020-12-22 11:17:31 +0100

    change the scan function to split on \r

--- command/command.go
+++ command/command.go
@@ -40,6 +40,17 @@ func Runner(l *log.Logger, args ...string) func(string) (bool, error) {
 		}
 
 		out := bufio.NewScanner(multi)
+		scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+			for i := 0; i < len(data); i++ {
+				if data[i] == '\r' || data[i] == '\n' {
+					return i + 1, data[:i], nil
+				}
+			}
+			if !atEOF {
+				return 0, nil, nil
+			}
+			return 0, data, bufio.ErrFinalToken
+		})
 
 		for out.Scan() {
 			l.Printf("%s", out.Text())
